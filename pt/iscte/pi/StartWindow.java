0ad21138d3121020060a5953866b42b85e7bd0ef
josefsantos <jfnfs@iscte-iul.pt>
2018-10-09 15:34:01 +0100

    Fisrt Window

---
+++ pt/iscte/pi/StartWindow.java
@@ -0,0 +1,70 @@
+package pt.iscte.pi;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+
+public class StartWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StartWindow window = new StartWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public StartWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 364, 513);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(StartWindow.class.getResource("/Images/Worms-United.jpg")));
+		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Quit");
+		btnNewButton.setBounds(99, 172, 127, 39);
+		panel.add(btnNewButton);
+		
+		JButton btnOptions = new JButton("Options");
+		btnOptions.setBounds(99, 120, 127, 39);
+		panel.add(btnOptions);
+		
+		JButton btnStartNewGame = new JButton("Start New Game");
+		btnStartNewGame.setBounds(99, 68, 127, 39);
+		panel.add(btnStartNewGame);
+	}
+}
